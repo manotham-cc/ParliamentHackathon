c60926b7cb9157b09fb1cbbdc58303400d628de7
Kimhan Jongjaidee <121301423+Deonmayon@users.noreply.github.com>
2024-08-06 23:02:27 +0700

    add ASR

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+require('dotenv').config();
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
 let data = new FormData();
-
-//data.append
+data.append('file', fs.createReadStream('./Test/30.mp4'));
 
 let config = {
-    method: 'post',
-    maxBodyLength: Infinity
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://api.iapp.co.th/asr/v3',
+  headers: { 
+    'apikey': process.env.ASR_API_KEY, 
+    ...data.getHeaders()
+  },
+  data : data
 };
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
